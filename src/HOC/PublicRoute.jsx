120d2e5fc334486f97d897b819d009d4be495e36
zhurakovska <n.sinawka@gmail.com>
2023-08-23 13:51:36 +0300

    Update PublicRoute.jsx

--- src/HOC/PublicRoute.jsx
+++ src/HOC/PublicRoute.jsx
@@ -7,7 +7,7 @@ import { selectIsLoggedIn } from 'redux/selectors';
 export const PublicRoute = ({ children }) => {
   const isLoggedIn = useSelector(selectIsLoggedIn);
   if (isLoggedIn) {
-    return <Navigate to="/" />;
+    return <Navigate to="/contacts" />;
   }
   return children;
 };
